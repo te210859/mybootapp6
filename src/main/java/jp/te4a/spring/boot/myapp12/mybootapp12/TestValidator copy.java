14db6f459c811e3beb7cc03c58b77a1bed2eb61a
te210859 <te210859@gmail.com>
2024-06-14 14:29:51 +0900

    課題12登録

---
+++ src/main/java/jp/te4a/spring/boot/myapp12/mybootapp12/TestValidator copy.java
@@ -0,0 +1,22 @@
+package jp.te4a.spring.boot.myapp12.mybootapp12;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import jp.te4a.spring.boot.myapp12.mybootapp12.TestValid.Valid;
+
+public class TestValidator implements ConstraintValidator<Valid,String>{
+	String param;
+	@Override
+	public void initialize(TestValid.Valid nv){ 
+		param =  nv.param(); 
+	}
+	@Override
+	public boolean isValid(String in,ConstraintValidatorContext cxt){
+		if(in == null){
+			return false;
+		}
+		System.out.println(in.equals(param));
+		return !in.equals(param);
+		}
+}
+
