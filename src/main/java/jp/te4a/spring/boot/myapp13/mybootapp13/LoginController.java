829c5b83a4c28c76ff0a4b72cfaa67bb2ca9297f
te210859 <te210859@gmail.com>
2024-06-18 11:06:30 +0900

    課題13登録

---
+++ src/main/java/jp/te4a/spring/boot/myapp13/mybootapp13/LoginController.java
@@ -0,0 +1,5 @@
+package jp.te4a.spring.boot.myapp13.mybootapp13;
+
+public class LoginController {
+    
+}
---
+++ src/main/java/jp/te4a/spring/boot/myapp13/mybootapp13/LoginUserDetails.java
@@ -0,0 +1,23 @@
+package jp.te4a.spring.boot.myapp13.mybootapp13;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import lombok.Data;
+
+@Data
+public class LoginUserDetails extends User {
+    private final UserBean user;
+    public LoginUserDetails(UserBean userBean,
+        boolean accountNonExpried, 
+        boolean credenttialsNonExpired, 
+        boolean accountNonLocked, 
+        Collection<GrantedAuthority> authorities) {
+        super(userBean.getUsername(), userBean.getPassword(),
+            true, true, true, true, authorities);
+        this.user = userBean;
+    }
+}
+
